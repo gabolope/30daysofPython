48a7ab25a85043e988fddf5fdf970d778a46bb14
Gabriel Alejandro López <78502447+gabolope@users.noreply.github.com>
2025-03-26 16:27:07 -0300

    Día 1

---
+++ day_1.py
@@ -0,0 +1,21 @@
+print(3+4) #adittion
+print(3-4) #subtraction
+print(3*4) #multiplication
+print(3%4) #modulus
+print(3/4) #division
+print(3**4) #exponential
+print(3//4) #floor division operator
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+
+print(type(['Gabriel', 'Python', 'Argentina', 32])) #list
+print(type({"name": "Gabriel", "lastname": "López"})) #dict
+print(type((26, "Marzo", 2025))) #tuple
+print(type({1, 2, 3, 4})) #set
+
+# Encontrar distancia euclideana de (2, 3) and (10, 8)
+print("Distancia euclediana: ")
+print(((2-10)**2+(3-8)**2)**0.5)
