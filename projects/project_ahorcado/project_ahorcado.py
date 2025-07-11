c5832cc3d6aef9cb333e6ab1f115c5b68c456a9c
Gabriel Alejandro López <78502447+gabolope@users.noreply.github.com>
2025-07-11 13:17:42 -0300

    Proyecto ahorcado
    Replaces static word list with random Spanish word from API, adds unidecode for accent handling, tracks user responses, and improves game flow including guess validation, attempt tracking, and win/loss conditions.

--- projects/project_ahorcado/project_ahorcado.py
+++ projects/project_ahorcado/project_ahorcado.py
@@ -1,28 +1,49 @@
-import random
+import requests
 import string
+from unidecode import unidecode
 
-word_list = ['maNzana', 'Banana', 'Pera']
+url = 'https://random-word-api.herokuapp.com/word?lang=es'
+word = requests.get(url).json()[0]
+
+word_len = len(word) 
 letters = list(string.ascii_lowercase)
+tries = word_len + 2
+placeholder = ''.join(['_ '] * word_len)
+responses = list()
 
-word = random.choice(word_list).lower()
-word_total = word
-
-for letter in word:
-    word = word.replace(letter, '_ ')
-print(word)
-print(word_total)
-
-print(f'Adivina la palabra:\n{word}')
-
-
+print(f'Adivina la palabra:\n{placeholder}\n')
 
 while True:
     try:
         response = input('Ingresa una letra: ').lower()
         if len(response) == 1:
-            if response in word:
-                pass
+            if response in letters:
+                letters.remove(response)
+                responses.append(response)
+                placeholder = ''.join(f'{c} ' if unidecode(c) in responses else '_ 'for c in word)
+                print(placeholder)
+                print(f'Respuestas: {responses}')
+                tries -= 1
+                if '_' not in placeholder:
+                    print(f'Acertaste!!!\nLa palabra era ¨{word}¨\n')
+                    break
+                print(f'Intentos restantes: {tries}')
+                if tries == 0:
+                    print(f'Te quedaste sin intentos. Perdiste.\nLa palabra era ¨{word}¨\n')
+                    break
+            else:
+                print('Ya probaste con esa letra.')
+                print(f'Intentos restantes: {tries}\n')
         else:
-            print('Ingresa una unica letra')
+            if unidecode(response) == unidecode(word):
+                print(f'Acertaste!!!\nLa palabra era ¨{word}¨\n')
+                break
+            else:
+                print('La palabra es incorrecta. ')
+                tries -= 1
+                print(f'Intentos restantes: {tries}')
+                if tries == 0:
+                    print(f'Te quedaste sin intentos. Perdiste.\nLa palabra era ¨{word}¨\n')
+                    break
     except ValueError:
-        print('No ingresaste una letra.')
+        print('No ingresaste una letra. \n')
