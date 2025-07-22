690884c8149172bd831ff6b2dee55645d8a752d9
Gabriel Alejandro López <78502447+gabolope@users.noreply.github.com>
2025-07-22 13:59:26 -0300

    dia 25

---
+++ day_25_pandas/day_25.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+# 1 Read the hacker_news.csv file from data directory
+df = pd.read_csv('./day_25_pandas/hacker_news.csv')
+
+# 2 Get the first five rows
+print(df.head)
+
+# 3 Get the last five rows
+print(df.tail)
+
+# 4 Get the title column as pandas series
+titles = df['title']
+print('\n ##################')
+print(titles)
+
+# 5 Count the number of rows and columns
+print(len(df.index))
+print(len(df.columns))
+print(df.shape)
+
+# Filter the titles which contain python
+print(df['python' in df['title']])
+# Filter the titles which contain JavaScript
+# Explore the data and make sense of it
