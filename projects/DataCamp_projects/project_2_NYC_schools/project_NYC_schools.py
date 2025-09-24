d6f9a507f2d102e9bee1066ca6cd07d9949c3c9e
Gabriel Alejandro López <78502447+gabolope@users.noreply.github.com>
2025-09-24 09:47:09 -0300

    Refine analysis of NYC schools SAT performance by borough
    Filters best_math_schools to only include schools with average_math above 80% of the max score. Improves borough SAT analysis by identifying the borough with the highest SAT standard deviation, counting its schools, and calculating its average and std of SAT scores, then outputs these metrics in a DataFrame.

--- projects/DataCamp_projects/project_2_NYC_schools/project_NYC_schools.py
+++ projects/DataCamp_projects/project_2_NYC_schools/project_NYC_schools.py
@@ -12,8 +12,9 @@ schools = pd.read_csv('projects\DataCamp_projects\project_2_NYC_schools\schools.
 # Which NYC schools have the best math results?
 # The best math results are at least 80% of the *maximum possible score of 800* for math.
 # Save your results in a pandas DataFrame called best_math_schools, including "school_name" and "average_math" columns, sorted by "average_math" in descending order.
+above_80 = schools[schools['average_math'] > 800 * 0.8]
 
-best_math_schools = schools.loc[:, ['school_name', 'average_math']].sort_values('average_math', ascending=False)
+best_math_schools = above_80.loc[:, ['school_name', 'average_math']].sort_values('average_math', ascending=False)
 print(best_math_schools)
 
 # What are the top 10 performing schools based on the combined SAT scores?
@@ -31,9 +32,36 @@ print(top_10_schools)
 # "average_SAT" - the mean of "total_SAT".
 # "std_SAT" - the standard deviation of "total_SAT".
 # Round all numeric values to two decimal places.
-borough_std = top_10_schools_complete.groupby('borough')['total_SAT'].std().sort_values(ascending=False)
-borough = pd.DataFrame(borough_std).iloc[0,0]
 
-num_schools = schools['borough'].value_counts().loc['Manhattan']
+# Agrupo por borough, calculo el std de SAT para cada grupo, ordeno en base al std 
+borough_std = top_10_schools_complete.groupby('borough')['total_SAT'].std().sort_values(ascending=False)
+
+# Quito el índex para acceder al nombre del primer elemento de la lista
+borough_std = borough_std.reset_index()
+
+# Tomo el primer elemento de la lista, 
+borough = borough_std.iloc[0,0]
 print(borough)
+
+# Realizo el conteo de cuántas escuelas hay por distrito, luego selecciono al distrito que tiene más std.
+num_schools = schools['borough'].value_counts().loc[borough]
 print(num_schools)
+
+# Calculo el promedio de total_SAT de todas las escuelas
+average_SAT = schools.groupby('borough')['total_SAT'].mean().__round__(2).loc[borough]
+print(average_SAT)
+
+# Calculo el std de SAT de todas las escuelas. 
+std_SAT = schools.groupby('borough')['total_SAT'].std().__round__(2).loc[borough]
+print(std_SAT)
+
+# Creo un dictionary para pasarlo a DF
+data = {
+  'borough': [borough],
+  'num_schools': [num_schools],
+  'average_SAT':[ average_SAT],
+  'std_SAT': [std_SAT]
+}
+
+largest_std_dev = pd.DataFrame(data)
+print(largest_std_dev)
