8446e33f436d9cec4ccb4b735ec39b6b46efd2f6
Gabriel Alejandro López <gabolope1517@gmail.com>
2025-10-16 15:06:43 -0300

    Clean and filter abnormal root length data
    Refactored data cleaning to adjust column types and remove abnormal roots (length < 10 after day 10) from the DataFrame. Improved plot axis labeling and tick generation for clarity.

--- projects/doctorado/raiz_largo/raiz_largo.py
+++ projects/doctorado/raiz_largo/raiz_largo.py
@@ -10,20 +10,27 @@ columnas_dias = [str(i) for i in range(1, 16)]
 
 largo_raiz = pd.melt(largo_raiz, id_vars=['genotipo', 'placa', 'experimento', 'id_planta'], value_vars = columnas_dias, var_name='dia', value_name='largo')
 
-# Borro los NaN y cambio los str a float
+# Borro los NaN
 largo_raiz = largo_raiz.replace('-', np.nan)
 largo_raiz = largo_raiz.dropna()
 
-largo_raiz['dia'] = pd.to_numeric(largo_raiz['dia'])
+# Ajusto los types de las columnas
+largo_raiz['dia'] = largo_raiz['dia'].astype(int)
 largo_raiz['largo'] = largo_raiz['largo'].astype(float)
+largo_raiz['id_planta'] = largo_raiz['id_planta'].astype(str)
+largo_raiz['placa'] = largo_raiz['placa'].astype(str)
 
-
+# Guardo el DF
 # largo_raiz.to_csv('projects/doctorado/raiz_largo/largo_raiz_pivot.csv', index=False)
 
+# Busco raíces anormales (largo menor de 10 después de los 10 días)
 filtro_anormales = ((largo_raiz['dia'] >= 10 ) & (largo_raiz['largo'] < 10))
 
-anormales = largo_raiz[filtro_anormales]['id_planta'].unique()
-print(anormales)
+anormales = largo_raiz[filtro_anormales]
+print(len(anormales))
+
+# quito las anormales del df largo_raiz
+largo_raiz = largo_raiz[~filtro_anormales]
 
 # Hago gráfico
 largo_raiz_14 = largo_raiz[largo_raiz['dia'] < 15] # saco el dia 15
@@ -33,7 +40,9 @@ plt.gca().invert_yaxis() #invierto el eje porque está al revés
 
 # Nombres en los ejes
 g.set(xlabel='Día', ylabel='Largo de raíz (mm)')
-plt.xticks(ticks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
-plt.ylim(0, None) # hago que la escala del y arranque en el 0
+# Ticks del x
+plt.xticks(ticks = range(1, 15))
+# hago que la escala Y arranca en el 0
+plt.ylim(0, None)
 
 plt.show()
