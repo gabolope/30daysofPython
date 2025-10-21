789c7d7db64183793b1b58386bf7516f38df4d1a
Gabriel Alejandro López <gabolope1517@gmail.com>
2025-10-21 14:03:38 -0300

    projecto

---
+++ projects/DataCamp_projects/project_4_crime/project_4_crime.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+crimes = pd.read_csv('projects/DataCamp_projects/project_4_crime/crimes.csv')
+
+# Which hour has the highest frequency of crimes? Store as an integer variable called peak_crime_hour.
+
+# paso la hora militar a str. zfill hace que todos los horarios tengan 4 caracteres de largo para poder ser formateados
+crimes['hour'] = pd.to_datetime(crimes['TIME OCC'].astype(str).str.zfill(4), format='%H%M').dt.hour
+
+peak_crime_hour = crimes['hour'].value_counts().index[0]
+print(f'La hora con más crímenes es: {peak_crime_hour}:00 hrs')
+
+
+# Which area has the largest frequency of night crimes (crimes committed between 10pm and 3:59am)? Save as a string variable called peak_night_crime_location.
+
+#hago un listado de las horas nocturnas y filtro en base a eso. 
+night = [22, 23, 24, 0, 1, 2, 3]
+
+crimes_night = crimes[crimes['hour'].isin(night)]
+print(crimes_night.head())
+
+peak_night_crime_location = crimes_night['AREA NAME'].value_counts().index[0]
+print(f'El área con más crímenes nocturnos es: {peak_night_crime_location}')
+
+
+# Identify the number of crimes committed against victims of different age groups. Save as a pandas Series called victim_ages, with age group labels "0-17", "18-25", "26-34", "35-44", "45-54", "55-64", and "65+" as the index and the frequency of crimes as the values.
+
+labels = ['0-17', '18-25', '26-34', '35-44', '45-54', '55-64', '65+']
+max_age = crimes['Vict Age'].max()
+bins = [0, 17, 25, 34, 44, 54, 64, max_age]
+
+crimes['age_group'] = pd.cut(crimes['Vict Age'], labels=labels, bins=bins)
+
+victim_ages = crimes['age_group'].value_counts()
+print(victim_ages)
--- projects/project_2_product_management/project_2_product_management.py
+++ projects/project_2_product_management/project_2_product_management.py
@@ -1,17 +1,53 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import datetime
 
-workout_df = pd.read_csv('datacamp/project_2_product_management/workout.csv')
-print(workout_df)
+workout = pd.read_csv('projects/project_2_product_management/workout.csv')
+keywords = pd.read_csv('projects/project_2_product_management/three_keywords.csv')
+workout_geo = pd.read_csv('projects/project_2_product_management/workout_geo.csv')
+keywords_geo = pd.read_csv('projects/project_2_product_management/three_keywords_geo.csv')
+
+
 # When was the global search for 'workout' at its peak? Save the year of peak interest as a string named year_str in the format "yyyy".
 
-# primero, hago una columna con todos los años sin tener en cuenta el mes. 
-def year_filter(year_month):
-    year_month_obj = datetime.strptime(year_month, '%Y-%m')
-    return year_month_obj.year
+workout['year'] = pd.to_datetime(workout['month'].astype(str), format='%Y-%m').dt.year
 
-workout_df['year'] = workout_df['month'].apply(year_filter)
+workout.sort_values(by='workout_worldwide', inplace=True)
 
-print(workout_df)
+year_str = str(workout['year'].iloc[0])
+print(f'Año con más búsquedas de workout: {year_str}')
+
+
+# Of the keywords available, what was the most popular during the covid pandemic, and what is the most popular now? Save your answers as variables called peak_covid and current respectively.
+
+keywords['year'] = pd.to_datetime(keywords['month'].astype(str), format='%Y-%m').dt.year
+
+covid_keywords = keywords[keywords['year'] == 2020]
+current_keywords = keywords[keywords['year'] == 2022]
+
+peak_covid = (covid_keywords[['home_workout_worldwide', 'gym_workout_worldwide', 'home_gym_worldwide']].max()).sort_values(ascending=False).index[0]
+
+current = (current_keywords[['home_workout_worldwide', 'gym_workout_worldwide', 'home_gym_worldwide']].max()).sort_values(ascending=False).index[0]
+
+peak_covid = peak_covid.replace('_worldwide', '')
+current = current.replace('_worldwide', '')
+
+print(f'Palabra más buscada en 2020: {peak_covid}. Y más buscada en 2022: {current}')
+
+
+# What country has the highest interest for workouts among the following: United States, Australia, or Japan? Save your answer as top_country.
+
+workout_uaj = workout_geo[workout_geo['country'].isin(['United States', 'Australia', 'Japan'])]
+
+top_country = workout_uaj['country'].iloc[0]
+
+print(f'País con más interes: {top_country}')
+
+
+# You'd be interested in expanding your virtual home workouts offering to either the Philippines or Malaysia. Which of the two countries has the highest interest in home workouts? Identify the country and save it as home_workout_geo.
+
+keywords_jm = keywords_geo[keywords_geo['Country'].isin(['Philippines', 'Malaysia'])].sort_values(by='home_workout_2018_2023')
+
+home_workout_geo = keywords_jm['Country'].iloc[0]
+
+print(home_workout_geo)
